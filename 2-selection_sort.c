be7155bf65158d76ed75a82fbb1411ece6f454c7
Maysummer <udojyk@gmail.com>
2022-07-20 16:11:31 -0700

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -25,9 +25,7 @@ void selection_sort(int *array, size_t size)
 			temp = array[minIndex];
 			array[minIndex] = array[i];
 			array[i] = temp;
+			print_array(array, size);
 		}
-		print_array(array, size);
 	}
 }
-
-
