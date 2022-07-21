dd5786033d38c8f3be0823a9510a1324aef6e019
Maysummer <udojyk@gmail.com>
2022-07-21 15:33:39 -0700

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ * quick-sort - function that sorts an array of integers
+ * in ascending order using the Quick sort algorithm
+ *
+ * @array: array to be sorted
+ * @size: size of array
+ */
+
+void quicksort(int number[25],int first,int last){
+   int i, j, pivot, temp;
+   if(first<last){
+      pivot=first;
+      i=first;
+      j=last;
+      while(i<j){
+         while(number[i]<=number[pivot]&&i<last)
+         i++;
+         while(number[j]>number[pivot])
+         j--;
+         if(i<j){
+            temp=number[i];
+            number[i]=number[j];
+            number[j]=temp;
+         }
+      }
+      temp=number[pivot];
+      number[pivot]=number[j];
+      number[j]=temp;
+      quicksort(number,first,j-1);
+      quicksort(number,j+1,last);
+   }
+}
